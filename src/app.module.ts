562907f7a19f4b2fb3805e9d986406207d7e12e4
Baraa <albaraa.m.m2005@gmail.com>
2026-05-27 18:30:22 +0300

    feat: add configuration management with Joi validation and environment variables for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
 
 const ENV = process.env.NODE_ENV;
 
@@ -19,19 +22,21 @@ const ENV = process.env.NODE_ENV;
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      validationSchema: environmentValidation,
+      load: [appConfig, databaseConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        port: +configService.get('DB_PORT'),
-        host: configService.get('DB_HOST'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        synchronize: configService.get('database.synchronize'),
+        port: configService.get('database.port'),
+        host: configService.get('database.host'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
       }),
     }),
     TagsModule,
---
+++ src/config/environment.validation.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  DB_PORT: Joi.number().port().default(5432),
+  DB_HOST: Joi.string().hostname().default('localhost'),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_SYNC: Joi.boolean().default(false),
+  DB_AUTOLOAD: Joi.boolean().default(false),
+});
